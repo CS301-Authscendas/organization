37da1e52d1e0d58f9be1427b0e616b8998dc7ec1
Daryl Wong <youcanfinddaryl@gmail.com>
2022-10-06 10:14:25 +0800

    update error messages

--- src/organization/organization.repository.ts
+++ src/organization/organization.repository.ts
@@ -36,7 +36,7 @@ export class OrganizationRepository {
     async createOrganization(org: Organization): Promise<void> {
         const found_org = await this.entityManager.findOne(Organization, { id: org.id });
         if (found_org) {
-            throw new BadRequestException("Organization with id already exists");
+            throw new BadRequestException("Organization with id: ".concat(org.id, " already exists"));
         }
         await this.entityManager.create(org);
     }
@@ -45,7 +45,7 @@ export class OrganizationRepository {
         // return await null;
         const found_org = await this.entityManager.findOne(Organization, { id: id });
         if (!found_org) {
-            throw new BadRequestException("Organization name does not exist");
+            throw new BadRequestException("Organization with id: ".concat(id, " does not exist"));
         }
         return found_org;
     }
@@ -53,17 +53,17 @@ export class OrganizationRepository {
     async updateOrganization(newOrg: Organization): Promise<void> {
         const found_org = await this.entityManager.findOne(Organization, { id: newOrg.id });
         if (!found_org) {
-            throw new BadRequestException("Organization with id does not exist");
+            throw new BadRequestException("Organization with id: ".concat(newOrg.id, " does not exist"));
         }
         const { id, ...orgDetails } = newOrg;
-        this.entityManager.update(Organization, { id }, plainToClass(Organization, orgDetails));
+        await this.entityManager.update(Organization, { id }, plainToClass(Organization, orgDetails));
     }
 
     async deleteOrganization(id: string): Promise<void> {
         const found_org = await this.entityManager.findOne(Organization, { id: id });
         if (!found_org) {
-            throw new BadRequestException("Organization with id does not exist");
+            throw new BadRequestException("Organization with id: ".concat(id, " does not exist"));
         }
-        this.entityManager.delete(Organization, { id: id });
+        await this.entityManager.delete(Organization, { id: id });
     }
 }
