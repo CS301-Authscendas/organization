c2436345bfb90b52a4ebaac1df030a4fe02453be
Daryl Wong <youcanfinddaryl@gmail.com>
2022-10-18 22:17:36 +0800

    added providers to test

--- src/s3/s3.service.spec.ts
+++ src/s3/s3.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { OrganizationModule } from "src/organization/organization.module";
+import { OrganizationService } from "src/organization/organization.service";
 import { UserModule } from "src/user/user.module";
+import { UserService } from "src/user/user.service";
 import { S3Service } from "./s3.service";
 
 describe("S3Service", () => {
@@ -9,7 +11,7 @@ describe("S3Service", () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             imports: [UserModule, OrganizationModule],
-            providers: [S3Service],
+            providers: [S3Service, UserService, OrganizationService],
         }).compile();
 
         service = module.get<S3Service>(S3Service);
